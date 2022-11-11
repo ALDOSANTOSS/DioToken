c445b13d3a005438863c49bd2c10cd23e493657d
adao <aldo.santana@gft.com>
2022-11-11 10:25:02 -0300

    Initial commit

---
+++ src/main/java/com/example/diotoken/DioTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.diotoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DioTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diotoken/DioTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diotoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
